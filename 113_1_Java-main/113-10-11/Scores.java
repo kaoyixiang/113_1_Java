297fd2f4f0b1464c02c3f0a961d4326175c317cc
kaoyixiang <a110223006@mail.shu.edu.tw>
2024-10-11 11:22:11 +0800

    113-10-11

---
+++ 113_1_Java-main/113-10-11/Scores.java
@@ -0,0 +1,9 @@
+public class Scores {
+    public static void main(String[] args) {
+        int[] scores = {88, 81, 74, 68, 78, 76, 77, 85, 95, 93};
+        
+        for(var i = 0; i < scores.length; i++) {
+            System.out.printf("�ǥͤ��ơG%d %n", scores[i]);
+        }
+    }
+}
